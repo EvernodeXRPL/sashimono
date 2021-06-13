c8e0e18082a344bf6808f88654ca5d8112f85316
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-06-13 16:48:13 +0530

    Simplification.

--- installer/install.sh
+++ installer/install.sh
@@ -2,31 +2,60 @@
 
 # Users that sashimono agent and rootless docker will operate under.
 sashimono_user=sashimono
+sashimono_user_dir=/home/$sashimono_user
+sashimono_agent_dir=$sashimono_user_dir/sashimono-agent
 docker_user=sashidocker
-setup_dir=$(pwd)/setupfiles
-sashimono_dir=/home/$sashimono_user/sashimono-agent
 docker_user_dir=/home/$docker_user
+dockerd_service=$dockerd_serviced
 
 # Check if users already exists.
 [ `id -u $sashimono_user 2>/dev/null || echo -1` -ge 0 ] && echo "User '$sashimono_user' already exists." && exit 1
 [ `id -u $docker_user 2>/dev/null || echo -1` -ge 0 ] && echo "User '$docker_user' already exists." && exit 1
 
-# Create users.
+# Install curl if not exists (required to download installation artifacts).
+[ ! $(command -v curl &> /dev/null) ] && sudo apt-get install -y curl
 
-sudo useradd -m $sashimono_user
-sudo usermod -L $sashimono_user # Prevent log in. 
-echo "Created '$sashimono_user' user."
 
+
+# --------------------------------------
+# Setup docker user and dockerd service.
+# --------------------------------------
 sudo useradd -m $docker_user
 sudo usermod -L $docker_user # Prevent log in.
 echo "Created '$docker_user' user."
 
-# Install curl if not exists (required to download installation artifacts).
-[ ! $(command -v curl &> /dev/null) ] && sudo apt-get install -y curl
-
 # Download and extract Docker rootless package.
 # This will extract the Docker rootless binaries at $docker_user_dir/bin
 curl --silent -fSL https://get.docker.com/rootless | sudo -u $docker_user sh > /dev/null
+
+# Setup rootless dockerd env variables.
+echo "export XDG_RUNTIME_DIR=$docker_user_dir/.docker/run" >> $docker_user_dir/.bashrc
+echo "export PATH=$docker_user_dir/bin:$PATH" >> $docker_user_dir/.bashrc
+echo "export DOCKER_HOST=unix://$docker_user_dir/.docker/run/docker.sock" >> $docker_user_dir/.bashrc
+
+# Configure dockerd service unit.
+sudo echo "[Unit]
+Description=Sashimono rootless dockerd service
+[Service]
+User=$docker_user
+WorkingDirectory=$docker_user_dir
+ExecStart=$docker_user_dir/bin/dockerd-rootless.sh
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/$dockerd_service.service
+sudo systemctl daemon-reload
+# Start the dockerd.
+sudo systemctl start $dockerd_service
+# Enable auto-start on bootup.
+sudo systemctl enable $dockerd_service
+
+
+
+# -------------------------------
+# Setup Sashimono user and agent.
+# -------------------------------
+sudo useradd -m $sashimono_user
+sudo usermod -L $sashimono_user # Prevent log in. 
+echo "Created '$sashimono_user' user."
 
 # Following two permissions are required for Sashimono to interact with the dockerd UNIX socket.
 # Add sashimono user to docker user group.
@@ -35,16 +64,5 @@ sudo usermod -a -G $docker_user $sashimono_user
 sudo -u $docker_user sh -c "mkdir -p $docker_user_dir/.docker/run"
 sudo chmod g+x $docker_user_dir/.docker/run
 
-# Setup dockerd run script.
-sudo cp $setup_dir/run-dockerd.sh $docker_user_dir/
-sudo chown $docker_user $docker_user_dir/run-dockerd.sh
-
-# Update service unit definition with physical values.
-sed -i 's?#run_as#?'"$docker_user"'?g' $setup_dir/sashimono-docker.service
-sed -i 's?#work_dir#?'"$docker_user_dir"'?g' $setup_dir/sashimono-docker.service
-
-# Configure dockerd service startup.
-sudo cp $setup_dir/sashimono-docker.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl start sashimono-docker
-sudo systemctl enable sashimono-docker
+# Setup docker client for sashimono user.
+sudo cp $docker_user_dir/bin/docker $sashimono_agent_dir/
--- installer/setupfiles/run-dockerd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-home_dir=$(realpath ~)
-
-# Add rootless docker env variables to .bashrc
-export XDG_RUNTIME_DIR=$home_dir/.docker/run
-export PATH=$home_dir/bin:$PATH
-export DOCKER_HOST=unix://$home_dir/.docker/run/docker.sock
-$home_dir/bin/dockerd-rootless.sh

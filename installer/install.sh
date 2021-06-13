544e6be75724d917734865d9c8fb82f7a2cf529f
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-06-13 11:25:03 +0530

    Minor fix.

--- installer/install.sh
+++ installer/install.sh
@@ -22,19 +22,19 @@ sudo usermod -L $docker_user # Prevent log in.
 echo "Created '$docker_user' user."
 
 # Install curl if not exists (required to download installation artifacts).
-[ ! command -v curl &> /dev/null ] && sudo apt-get install -y curl
+[ ! $(command -v curl &> /dev/null) ] && sudo apt-get install -y curl
 
 # Download and extract Docker rootless package.
 # This will extract the Docker rootless binaries at $docker_dir/bin
 curl --silent -fSL https://get.docker.com/rootless | sudo -u $docker_user sh > /dev/null
 
-# Update service unit definition with physical values.
-sed -i 's?#run_as#?'"$docker_user"'?g' $setup_dir/sashimono-docker.service
-sed -i 's?#docker_dir#?'"$docker_dir"'?g' $setup_dir/sashimono-docker.service
-
 # Set user permissions.
 sudo cp $setup_dir/run-dockerd.sh $docker_dir/
 sudo chown $docker_user $docker_dir/run-dockerd.sh
+
+# Update service unit definition with physical values.
+sed -i 's?#run_as#?'"$docker_user"'?g' $setup_dir/sashimono-docker.service
+sed -i 's?#docker_dir#?'"$docker_dir"'?g' $setup_dir/sashimono-docker.service
 
 # Configure dockerd service startup.
 sudo cp $setup_dir/sashimono-docker.service /etc/systemd/system/

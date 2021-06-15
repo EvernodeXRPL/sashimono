e738a1a632cdb3b449624197b016bb2650d69752
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-06-15 15:20:16 +0530

    Used rootless dockerd service.

--- installer/install.sh
+++ installer/install.sh
@@ -6,8 +6,6 @@ sashimono_user_dir=/home/$sashimono_user
 sashimono_agent_dir=$sashimono_user_dir/sashimono-agent
 dockerd_user=sashidockerd
 dockerd_user_dir=/home/$dockerd_user
-dockerd_service=sashimono-dockerd
-dockerd_socket=unix://$dockerd_user_dir/.docker/run/docker.sock
 
 # Check if users already exists.
 [ `id -u $sashimono_user 2>/dev/null || echo -1` -ge 0 ] && echo "User '$sashimono_user' already exists." && exit 1
@@ -22,36 +20,24 @@ dockerd_socket=unix://$dockerd_user_dir/.docker/run/docker.sock
 # --------------------------------------
 sudo useradd --shell /usr/sbin/nologin -m $dockerd_user
 sudo usermod --lock $dockerd_user
+sudo loginctl enable-linger $dockerd_user # Enable lingering to support rootless dockerd service installation.
 echo "Created '$dockerd_user' user."
 
-# Download and extract Docker rootless package.
-# This will extract the Docker rootless binaries at $dockerd_user_dir/bin
-curl --silent -fSL https://get.docker.com/rootless | sudo -u $dockerd_user sh > /dev/null
-echo "Installed rootless dockerd at $dockerd_user_dir/bin"
+dockerd_user_runtime_dir=/run/user/$(id -u $dockerd_user)
+dockerd_socket=unix://$dockerd_user_runtime_dir/.docker/run/docker.sock
 
-# Setup dockerd rootless launcher script.
-# (Using --icc=false for docker daemon to prevent inter-container communication)
-echo "#!/bin/sh
-export XDG_RUNTIME_DIR=$dockerd_user_dir/.docker/run
+# Create new daemon config to disable dockerd inter-container communication.
+echo '{"icc":false}' | sudo -u $dockerd_user tee $dockerd_user_dir/.config/docker/daemon.json >/dev/null
+
+# Download and install rootless dockerd.
+sudo loginctl enable-linger $dockerd_user
+curl --silent -fSL https://get.docker.com/rootless | sudo -u $dockerd_user XDG_RUNTIME_DIR=$dockerd_user_runtime_dir sh > /dev/null
+echo "Installed rootless dockerd."
+
+# Setup dockerd env variables for dockerd user for conveniance.
+echo "
 export PATH=$dockerd_user_dir/bin:\$PATH
-export DOCKER_HOST=$dockerd_socket
-$dockerd_user_dir/bin/dockerd-rootless.sh --icc=false" | sudo -u $dockerd_user tee $dockerd_user_dir/bin/run.sh >/dev/null
-sudo chmod +x $dockerd_user_dir/bin/run.sh
-
-# Configure dockerd service unit.
-sudo echo "[Unit]
-Description=Sashimono rootless dockerd service
-[Service]
-User=$dockerd_user
-ExecStart=$dockerd_user_dir/bin/run.sh
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/$dockerd_service.service
-sudo systemctl daemon-reload
-# Start the dockerd.
-sudo systemctl start $dockerd_service
-# Enable auto-start on bootup.
-sudo systemctl enable $dockerd_service
-echo "Configured $dockerd_service service."
+export DOCKER_HOST=$dockerd_socket" >>$dockerd_user_dir/.bashrc
 
 
 # --------------------------------------
@@ -64,9 +50,8 @@ echo "Created '$sashimono_user' user."
 # Following two permissions are required for Sashimono to interact with the dockerd UNIX socket.
 # Add sashimono user to docker user group.
 sudo usermod -a -G $dockerd_user $sashimono_user
-# Create docker run directory and assign execute permission for group.
-sudo -u $dockerd_user sh -c "mkdir -p $dockerd_user_dir/.docker/run"
-sudo chmod g+x $dockerd_user_dir/.docker/run
+# Assign group execute permission for group dockerd runtime dir.
+sudo chmod g+x $dockerd_user_runtime_dir
 
 # Setup sashimono agent directory.
 sudo mkdir -p $sashimono_agent_dir
--- installer/uninstall.sh
+++ installer/uninstall.sh
@@ -2,27 +2,25 @@
 # Sashimono agent uninstall script.
 
 sashimono_user=sashimono
-docker_user=sashidockerd
-dockerd_service=sashimono-dockerd
+dockerd_user=sashidockerd
+dockerd_user_dir=/home/$dockerd_user
 
-# Stop and uninstall our systemd services.
-echo "Stopping $dockerd_service service..."
-sudo systemctl stop $dockerd_service
-sudo systemctl disable $dockerd_service
-sudo rm /etc/systemd/system/$dockerd_service.service
-sudo systemctl daemon-reload
-sudo systemctl reset-failed
+# Uninstall rootless dockerd.
+echo "Uninstalling rootless dockerd..."
+sudo -u $dockerd_user $dockerd_user_dir/bin/dockerd-rootless-setuptool.sh uninstall
+sudo -u $dockerd_user $dockerd_user_dir/bin/rootlesskit rm -rf $dockerd_user_dir/.local/share/docker
 
 # Kill all processes for users.
-echo "Killing any user processes..."
+echo "Killing user processes..."
+sudo loginctl disable-linger $dockerd_user
+sudo pkill -SIGKILL -u $dockerd_user
 sudo pkill -SIGKILL -u $sashimono_user
-sudo pkill -SIGKILL -u $docker_user
 
 echo "Deleting users..."
+sudo userdel $dockerd_user
 sudo userdel $sashimono_user
-sudo userdel $docker_user
 
+sudo rm -r /home/$dockerd_user
 sudo rm -r /home/$sashimono_user
-sudo rm -r /home/$docker_user
 
 echo "Done."

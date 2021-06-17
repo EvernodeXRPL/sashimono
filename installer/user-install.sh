f2e70be7f6e7dedd2ea407dceccd51b434e0f0e9
Ravin Perera <33562092+ravinsp@users.noreply.github.com>
2021-06-17 13:47:06 +0530

    Per user dockerd installation scripts. (#13)

---
+++ installer/user-install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Sashimono contract instance user installation script.
+# This is intended to be called by Sashimono agent.
+
+# $1 - A number with 25 or less digits.
+uid=$1
+[ -z "$uid" ] && echo "ARGS,INST_ERR" && exit 1
+[ ${#uid} -gt 25 ] && echo "ARGS,INST_ERR" && exit 1
+[[ "$uid" =~ [^0-9] ]] && echo "ARGS,INST_ERR" && exit 1
+
+user="sashi$uid"
+user_dir=/home/$user
+docker_bin=/usr/bin/sashimono-dockerbin
+
+# Check if users already exists.
+[ `id -u $user 2>/dev/null || echo -1` -ge 0 ] && echo "HAS_USER,INST_ERR" && exit 1
+
+function rollback() {
+    echo "Rolling back user installation. $1"
+    $(pwd)/user-uninstall.sh $uid
+    echo "Rolled back the installation."
+    echo "$1,INST_ERR" && exit 1
+}
+
+# Setup user and dockerd service.
+useradd --shell /usr/sbin/nologin -m $user
+usermod --lock $user
+loginctl enable-linger $user # Enable lingering to support rootless dockerd service installation.
+chmod o-rwx $user_dir
+echo "Created '$user' user."
+
+user_id=$(id -u $user)
+user_runtime_dir="/run/user/$user_id"
+dockerd_socket="unix://$user_runtime_dir/docker.sock"
+
+# Setup env variables for the user.
+echo "
+export XDG_RUNTIME_DIR=$user_runtime_dir
+export PATH=$docker_bin:\$PATH
+export DOCKER_HOST=$dockerd_socket" >>$user_dir/.bashrc
+echo "Updated user .bashrc."
+
+# Wait until user systemd is functionning.
+user_systemd=""
+for (( i=0; i<30; i++ ))
+do
+    sleep 0.1
+    user_systemd=$(sudo -u $user XDG_RUNTIME_DIR=$user_runtime_dir systemctl --user is-system-running 2>/dev/null)
+    [ "$user_systemd" == "running" ] && break
+done
+[ "$user_systemd" != "running" ] && rollback "NO_SYSTEMD"
+
+echo "Installing rootless dockerd for user."
+sudo -u $user bash -i -c "$docker_bin/dockerd-rootless-setuptool.sh install"
+
+svcstat=$(sudo -u $user XDG_RUNTIME_DIR=$user_runtime_dir systemctl --user is-active docker.service)
+[ "$svcstat" != "active" ] && rollback "NO_DOCKERSVC"
+
+echo "Installed rootless dockerd."
+echo "$user_id,$user,$dockerd_socket,INST_SUC"
+exit 0
---
+++ installer/user-uninstall.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Sashimono contract instance user uninstall script.
+# This is intended to be called by Sashimono agent or via the user-install script for rollback.
+
+# $1 - A number with 25 or less digits.
+uid=$1
+[ -z "$uid" ] && echo "ARGS,UNINST_ERR" && exit 1
+[ ${#1} -gt 25 ] && echo "ARGS,UNINST_ERR" && exit 1
+[[ "$uid" =~ [^0-9] ]] && echo "ARGS,UNINST_ERR" && exit 1
+
+user="sashi$uid"
+user_dir=/home/$user
+docker_bin=/usr/bin/sashimono-dockerbin
+
+# Check if users exists.
+if [[ `id -u $user 2>/dev/null || echo -1` -ge 0 ]]; then
+        :
+else
+        echo "NO_USER,UNINST_ERR"
+        exit 1
+fi
+
+# Uninstall rootless dockerd.
+echo "Uninstalling rootless dockerd."
+sudo -u $user bash -i -c "$docker_bin/dockerd-rootless-setuptool.sh uninstall"
+echo "Removing rootless docker data."
+sudo -u $user $docker_bin/rootlesskit rm -rf $user_dir/.local/share/docker
+
+# Gracefully terminate user processes.
+echo "Terminating user processes."
+loginctl disable-linger $user
+pkill -SIGINT -u $user
+sleep 0.5
+echo "Unmounting user filesystems."
+fsmounts=$(cat /proc/mounts | cut -d ' ' -f 2 | grep "/home/$user")
+readarray -t mntarr <<<"$fsmounts"
+for mnt in "${mntarr[@]}"
+do
+   [ -z "$mnt" ] || umount $mnt
+done
+
+# Force kill user processes.
+procs=$(ps -U root 2>/dev/null | wc -l)
+if [ "$procs" != "0" ]; then
+
+    # Wait for some time and check again.
+    sleep 1
+    procs=$(ps -U root 2>/dev/null | wc -l)
+    if [ "$procs" != "0" ]; then
+        echo "Force killing user processes."
+        pkill -SIGKILL -u $user
+    fi
+
+fi
+
+echo "Deleting user."
+userdel $user
+rm -r /home/$user
+
+[ -d /home/$user ] && echo "NOT_CLEAN,UNINST_ERR" && exit 1
+
+echo "UNINST_SUC"
+exit 0

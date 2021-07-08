2dbb749c2c5a74852ccf968115b6ebbcfb9c3505
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-07-08 17:20:06 +0530

    Install script fixes.

--- installer/sashimono-install.sh
+++ installer/sashimono-install.sh
@@ -25,18 +25,6 @@ fi
 # Install Sashimono agent binaries into sashimono bin dir.
 # TODO.
 
-# Copy necessary files into sashimono data folder.
-cp -r ../dependencies/default_contract $sashimono_data
-if ! cp ../bootstrap-contract/script.sh $sashimono_data/default_contract/contract_fs/seed/state/script.sh; then
-    echo "script.sh file not found."
-    exit 1
-fi
-
-if ! cp ../build/bootstrap_contract $sashimono_data/default_contract/contract_fs/seed/state/bootstrap_contract; then
-    echo "bootstrap_contract file not found." 
-    exit 1
-fi
-
 # Download docker packages into a tmp dir and extract into docker bin.
 echo "Installing rootless docker packages into $docker_bin"
 tmp=$(mktemp -d)

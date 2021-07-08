b36bf1e122b493d3ab4a30a6880222294574d41b
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-07-08 22:16:59 +0530

    Name fix.

--- src/conf.hpp
+++ src/conf.hpp
@@ -81,7 +81,7 @@ namespace conf
     struct sa_context
     {
         std::string command;                // The CLI command issued to launch Sashimono agent
-        std::string exe_dir;                // Hot Pocket executable dir.
+        std::string exe_dir;                // Sashimono Agent executable dir.
         std::string hpws_exe_path;          // hpws executable file path.
         std::string hpfs_exe_path;          // hpfs executable file path.
         std::string contract_template_path; // Path to default contract.

225af75d121d7c492c99c25a2fb4d3eaec3183ba
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-07-09 21:43:17 +0530

    Docker install fix.

--- dependencies/docker-install.sh
+++ dependencies/docker-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Rootless docker installation script.
 
-docker_bin=$1
+docker_bin=$(realpath $1)
 
 # Silently exit if docker_bin is not empty.
 [ ! -z "$(ls -A $docker_bin 2>/dev/null)" ] && exit 0
@@ -11,6 +11,7 @@ mkdir -p $docker_bin
 # Download docker packages into a tmp dir and extract into docker bin.
 echo "Installing rootless docker packages into $docker_bin"
 
+tmp=$(mktemp -d)
 cd $tmp
 curl https://download.docker.com/linux/static/stable/x86_64/docker-20.10.7.tgz --output docker.tgz
 curl https://download.docker.com/linux/static/stable/x86_64/docker-rootless-extras-20.10.7.tgz --output rootless.tgz

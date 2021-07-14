521423c20df899007d5bcd8d061de911d8af3e64
Savinda Senevirathne <savindadilsara@gmail.com>
2021-07-14 12:55:52 +0530

    Apply latest hp.cfg values during hpfs service starts. (#29)

--- src/hpfs_manager.hpp
+++ src/hpfs_manager.hpp
@@ -7,5 +7,6 @@ namespace hpfs
 {
     int start_hpfs_systemd(const std::string &username);
     int stop_hpfs_systemd(const std::string &username);
+    int update_service_conf(const std::string &username, const std::string &log_level, const bool is_full_history);
 } // namespace hpfs
 #endif

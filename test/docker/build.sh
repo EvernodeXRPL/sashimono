da9d75fd8219ab0f4b6ed36956312123d861987a
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-09-20 16:57:15 +0530

    Renamed docker images to use hp-latest image.

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -2,5 +2,5 @@
 
 img=hotpocketdev/sashimono
 
-docker build -t $img:hp.0.5-ubt.20.04 -f ./Dockerfile.ubt.20.04 .
-docker build -t $img:hp.0.5-ubt.20.04-njs.14 -f ./Dockerfile.ubt.20.04-njs .
+docker build -t $img:hp.latest-ubt.20.04 -f ./Dockerfile.ubt.20.04 .
+docker build -t $img:hp.latest-ubt.20.04-njs.14 -f ./Dockerfile.ubt.20.04-njs .

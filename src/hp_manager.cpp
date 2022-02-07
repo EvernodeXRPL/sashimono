ed3e1ef2aaba7a12b3a762c5c848b44aa002cdaf
chalith <desaman.chalith@gmail.com>
2022-02-07 16:52:48 +0530

    Revert test timeout

--- src/hp_manager.cpp
+++ src/hp_manager.cpp
@@ -15,7 +15,7 @@ namespace hp
 
     constexpr int FILE_PERMS = 0644;
     constexpr int MAX_UNIQUE_NAME_RETRIES = 10;     // Max retries before abandoning container uniqueness check.
-    constexpr int DOCKER_CREATE_TIMEOUT_SECS = 600; // Max timeout for docker create command to execute.
+    constexpr int DOCKER_CREATE_TIMEOUT_SECS = 120; // Max timeout for docker create command to execute.
 
     sqlite3 *db = NULL; // Database connection for hp related sqlite stuff.
 

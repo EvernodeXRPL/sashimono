c7f46693b29f59ff89a244c5edd856f80d0019de
ravinsp <33562092+ravinsp@users.noreply.github.com>
2022-03-07 18:57:30 +0530

    Used evernodedev docker account.

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-img=hotpocketdev/sashimono
+img=evernodedev/sashimono
 
 docker build -t $img:hp.latest-ubt.20.04 -f ./Dockerfile.ubt.20.04 .
 docker build -t $img:hp.latest-ubt.20.04-njs.14 -f ./Dockerfile.ubt.20.04-njs .

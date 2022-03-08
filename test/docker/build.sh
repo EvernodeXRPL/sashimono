d54b8b7902c1b1462a8d1cd2af421cbea4933919
ravinsp <33562092+ravinsp@users.noreply.github.com>
2022-03-08 19:18:27 +0530

    nodejs 16 upgrade.

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -3,4 +3,4 @@
 img=evernodedev/sashimono
 
 docker build -t $img:hp.latest-ubt.20.04 -f ./Dockerfile.ubt.20.04 .
-docker build -t $img:hp.latest-ubt.20.04-njs.14 -f ./Dockerfile.ubt.20.04-njs .
+docker build -t $img:hp.latest-ubt.20.04-njs.16 -f ./Dockerfile.ubt.20.04-njs .

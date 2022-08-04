efc0410baa8c9dfa5c1968ba4c7f5ca1850dc6d0
chalith <desaman.chalith@gmail.com>
2022-08-04 13:16:07 +0530

    Updated sashimono and message board versions

--- src/version.hpp
+++ src/version.hpp
@@ -6,7 +6,7 @@
 namespace version
 {
     // Sashimono agent version. Written to new configs.
-    constexpr const char *AGENT_VERSION = "0.5.6";
+    constexpr const char *AGENT_VERSION = "0.5.7";
 
     // Minimum compatible config version (this will be used to validate configs).
     constexpr const char *MIN_CONFIG_VERSION = "0.5.0";

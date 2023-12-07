97ab49a8b3ea021e522286d5d5b92ebbd3dfcd1e
Kithmini Gunawardhana <kithminishanika@gmail.com>
2023-12-07 20:21:48 +0530

    Changed docker id to evernode (#307)

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-img=evernodedev/sashimono
+img=evernode/sashimono
 
 docker build -t $img:hp.latest-ubt.20.04 -t $img:hp.0.6.4-ubt.20.04 -f ./Dockerfile.ubt.20.04 .
 docker build -t $img:hp.latest-ubt.20.04-njs.20 -t $img:hp.0.6.4-ubt.20.04-njs.20 -f ./Dockerfile.ubt.20.04-njs .

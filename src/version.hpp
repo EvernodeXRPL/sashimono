c158d9ea2cd4430252372ad698505061abdd139e
chalith <desaman.chalith@gmail.com>
2024-02-09 14:13:38 +0530

    Updated version number

--- src/version.hpp
+++ src/version.hpp
@@ -6,7 +6,7 @@
 namespace version
 {
     // Sashimono agent version. Written to new configs.
-    constexpr const char *AGENT_VERSION = "0.8.1";
+    constexpr const char *AGENT_VERSION = "0.8.2";
 
     // Minimum compatible config version (this will be used to validate configs).
     constexpr const char *MIN_CONFIG_VERSION = "0.5.0";

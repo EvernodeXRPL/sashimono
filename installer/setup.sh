9a4f2af1bc87fa126947b1093f851a73af4e5953
chalith <desaman.chalith@gmail.com>
2024-05-16 16:03:33 +0530

    Changed default network

--- installer/setup.sh
+++ installer/setup.sh
@@ -99,8 +99,7 @@
     export MIN_OPERATIONAL_DURATION=3
     export MIN_REPUTATION_COST_PER_MONTH=10
 
-    #export NETWORK="${NETWORK:-mainnet}"
-    export NETWORK="${NETWORK:-devnet}"
+    export NETWORK="${NETWORK:-mainnet}"
 
     # Private docker registry (not used for now)
     export DOCKER_REGISTRY_USER="sashidockerreg"

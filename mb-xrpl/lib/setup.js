697318576b038911bcceea88b598456287f704cc
chalith <desaman.chalith@gmail.com>
2024-07-17 15:14:45 +0530

    Show reputation in evernode status

--- mb-xrpl/lib/setup.js
+++ mb-xrpl/lib/setup.js
@@ -510,6 +510,7 @@ class Setup {
                     }
                 }
                 console.log(`\nHost status: ${hostInfo.active ? 'active' : 'inactive'}`);
+                console.log(`\nReputation: ${hostInfo.hostReputation}`);
                 console.log(`\nCountry code: ${hostInfo.countryCode}`);
 
                 await hostClient.disconnect();
